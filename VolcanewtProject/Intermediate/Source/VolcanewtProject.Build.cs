6b232b2aff0daa7b9c399abe73ebdb301bf93277
Hightemplarr <Hightemplarr@knights.ucf.edu>
2023-10-13 19:56:25 -0400

    Small Edit

---
+++ VolcanewtProject/Intermediate/Source/VolcanewtProject.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class VolcanewtProject : ModuleRules
+{
+	public VolcanewtProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
